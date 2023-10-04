cbf4ba7d22e90d3b7c8b3f21eb39eae989cd7fa0
Aishen <aish3n@pm.me>
2023-10-04 10:24:52 +0800

    frontend: refactor code

--- frontend/main.js
+++ frontend/main.js
@@ -1,58 +1,41 @@
 import * as web3 from "@solana/web3.js";
 import * as anchor from "@coral-xyz/anchor";
-import buffer from "buffer";
 
 async function main() {
     const walletProvider = getProvider();
     const anchorProgram = await getProgram(walletProvider);
 
-    await walletProvider.connect({ onlyIfTrusted: true });
+    await walletProvider.connect({ onlyIfTrusted: true }).catch(() => {});
     await createEvents(walletProvider, anchorProgram);
 }
 
-async function createEvents(walletProvider, anchorProgram) {
-    const connectButton = document.getElementById("connect-wallet");
-    const disconnectButton = document.getElementById("disconnect-wallet");
-    const initializeNewAccountButton = document.getElementById("initialize-button");
+async function createEvents(walletProvider, _anchorProgram) {
+    const toggleConnectButton = document.getElementById("connect-wallet");
+    const buttonText = walletProvider.isConnected ? "Disconnect" : "Connect"
 
-    connectButton.addEventListener("click", async () => {
-        await walletProvider.connect();
-    });
+    toggleConnectButton.innerText = `${buttonText} Wallet`;
+    toggleConnectButton.addEventListener("click", async () => {
+        if (walletProvider.isConnected) {
+            await walletProvider.disconnect();
+            toggleConnectButton.innerText = "Connect Wallet";
 
-    disconnectButton.addEventListener("click", async () => {
-        await walletProvider.disconnect();
-    });
-
-    initializeNewAccountButton.addEventListener("click", async () => {
-        const keypair = anchor.web3.Keypair.generate();
-        const options = {
-            data: keypair.publicKey,
-            signer: walletProvider.publicKey,
-            systemProgram: anchor.web3.SystemProgram.programId,
+            return;
         }
 
-        const tx = await anchorProgram
-            .methods
-            .initialize("hello")
-            .accounts(options)
-            .signers([keypair])
-            .rpc();
-
-        console.log("Your transaction signature", tx);
-        console.log(await anchorProgram.account.data.all());
-    })
+        await walletProvider.connect();
+        toggleConnectButton.innerText = "Disconnect Wallet";
+    });
 }
 
 async function getProgram(walletProvider) {
-    window.Buffer = window.Buffer || buffer.Buffer;
-
     const options = anchor.AnchorProvider.defaultOptions();
     const connection = new web3.Connection(web3.clusterApiUrl("devnet"), "confirmed");
     const anchorProvider = new anchor.AnchorProvider(connection, walletProvider, options);
+    const programId = "HDYyTAVBJL1JMp3kUukxy784micPpWRMBjBZF9zJQ1cX";
 
     return new anchor.Program(
-        await anchor.Program.fetchIdl("HDYyTAVBJL1JMp3kUukxy784micPpWRMBjBZF9zJQ1cX", anchorProvider),
-        new web3.PublicKey("HDYyTAVBJL1JMp3kUukxy784micPpWRMBjBZF9zJQ1cX"),
+        await anchor.Program.fetchIdl(programId, anchorProvider),
+        new web3.PublicKey(programId),
         anchorProvider
     );
 }
